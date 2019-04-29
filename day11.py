883796bd2c8929d7444c83ecc129f6f290a7f4e1
KyleXID <assaboy112@gmail.com>
2019-04-29 11:49:33 +0900

    Day11

---
+++ day11.py
@@ -0,0 +1,46 @@
+def complexNumberMultiply(a, b):
+  plus = a.index("+")
+  plus2 = b.index("+")
+  
+  num1 = int(a[:plus])
+  num2 = int(a[(plus+1):(len(a)-1)])
+  num3 = int(b[:plus2])
+  num4 = int(b[(plus2+1):(len(b)-1)])
+  
+  com1 = str((num1 * num3) - (num2 * num4))
+  com2 = str((num2 * num3) + (num1 * num4))
+  
+  result = com1 + "+" + com2 + "i"
+  
+  return result
+  
+print(complexNumberMultiply("1+1i","1+1i"))
+
+# 짧은 버전
+#def complexNumberMultiply2(a, b):
+#  """
+#  :type a: str
+#  :type b: str
+#  :rtype: str
+#  """
+#  a1, a2 = map(int, a[:-1].split('+'))
+#  b1, b2 = map(int, b[:-1].split('+'))
+#  return '%d+%di' % (a1 * b1 - a2 * b2, a1 * b2 + a2 * b1)
+
+# 다른 버전
+#def complexNumberMultiply(a, b):
+#  """
+#  :type a: str
+#  :type b: str
+#  :rtype: str
+#  """
+#
+#  firstArr = a[:-1].split('+')
+#  a1 = int(firstArr[0])
+#  a2 = int(firstArr[1])
+#
+#  secondArr = b[:-1].split('+')
+#  b1 = int(secondArr[0])
+#  b2 = int(secondArr[1])
+#
+#  return f'{a1 * b1 - a2 * b2}+{a1 * b2 + a2 * b1}i'
