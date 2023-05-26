b7eaf192da746b10e26692f9f1c50daff1f065d4
이형주 <49135198+KyleXID@users.noreply.github.com>
2023-05-26 11:19:02 +0900

    propose 1.4.2rc4

--- 20190610.py
+++ 20190610.py
@@ -3,7 +3,7 @@ def solution(array, commands):
     answer = []
     for x in commands:
         arr = array[x[0]-1:x[1]]
-        arr.sort()
+        arr.sort()  # test
         arr2 = arr[x[2]-1]
         answer.append(arr2)
     return answer
